8b02b908f72361d2dbd3b23cab8113483c254947
Roberto Guzman <robertoguzman@MacBook-Pro-de-Roberto-2.local>
2021-09-06 13:48:32 -0500

    add jwt

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+// LIB DE JWT
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token invalido']);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token caducado']);
+            } else {
+                return response()->json(['status' => 'Token No autorizado']);
+            }
+        }
+        return $next($request);
+
+
+        // return $next($request);
+    }
+}
